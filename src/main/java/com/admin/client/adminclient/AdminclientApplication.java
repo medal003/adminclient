f351a6eb32d49607c93a33336be6105c1e73c740
meideng.zh <meideng.zh@devops.box>
2018-11-24 11:31:34 +0800

    Initial commit-2018年11月24日admin客户端

---
+++ src/main/java/com/admin/client/adminclient/AdminclientApplication.java
@@ -0,0 +1,12 @@
+package com.admin.client.adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminclientApplication.class, args);
+    }
+}
